30e4009281fe538b9f82b5f2e4b2554f62bfd92d
Matt_Czerwinsky <matt.czerwinsky@gmail.com>
2019-05-19 21:41:22 +0100

    poprawka poprzedniego commita

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,12 +7,9 @@ from .models import Question
 #wkleilem aktualizacje widoku index z 3 czesci tutoriala
 #zmiana konczy sie na return templaterender
 def index(request):
-     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
