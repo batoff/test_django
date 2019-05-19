3ca2cb49de14a992b9644817061688fa9a4c7554
Matt_Czerwinsky <matt.czerwinsky@gmail.com>
2019-05-19 21:36:49 +0100

    aktualizacja widoku index aby uzywal szablonu

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,14 +1,18 @@
-# uwaga zmieniłam to:
+﻿# uwaga zmieniłam to:
 from django.http import HttpResponse
 from django.template import loader
 
 from .models import Question
 
-
+#wkleilem aktualizacje widoku index z 3 czesci tutoriala
+#zmiana konczy sie na return templaterender
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
