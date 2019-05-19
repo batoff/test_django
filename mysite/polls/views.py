6545930e6e089579b5d1c3440d194334ba332034
Anna Marciniak <fazuszka@gmail.com>
2019-05-19 09:45:57 +0000

    tutorial część 3 "pisanie widoków które faktycznie coś robią"

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,6 +4,14 @@ def index(request):
     return HttpResponse("Hello world. You're at the polls index.")
 
 # uwaga dodałam to:
+from .models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
