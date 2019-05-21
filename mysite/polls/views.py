a2f5b14b0f756b9d636d8c95470bb3c4f2141d4b
Matt_Czerwinsky <matt.czerwinsky@gmail.com>
2019-05-21 05:39:04 +0100

    Dodany fragment o error 404:

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,7 @@
 ﻿# uwaga zmieniłam to:
 from django.http import HttpResponse
+#dodana linia o 404
+from django.shortcuts import get_object_or_404, render
 from django.template import loader
 
 from .models import Question
@@ -11,6 +13,10 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
+#dodane 3 linie o 404
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
